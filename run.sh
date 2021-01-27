dc9223314fb37f1656d6cf9e7136bc6c311fd857
lukma99 <lukas.maikowski@ymail.com>
2021-01-27 15:05:49 +0100

    code optimizations in run script

--- run.sh
+++ run.sh
@@ -2,25 +2,17 @@
 
 NEW_ARGS=""
 COMMAND=""
-CURRENT_CONTEXT=$(kubectl config current-context)
-CURRENT_NAMESPACE=$(kubectl config view --minify | grep namespace | cut -d ':' -f 2 | sed 's/^[ \t]*//;s/[ \t]*$//')
-
 HELM_SUDO_PROMPT=${HELM_SUDO_PROMPT:-false}
+
 if [ "$HELM_SUDO_PROMPT" = true ]; then
+    CURRENT_CONTEXT=$(kubectl config current-context)
+    CURRENT_NAMESPACE=$(kubectl config view --minify | grep namespace | cut -d ':' -f 2 | sed 's/^[ \t]*//;s/[ \t]*$//')
     read -p "WARNING: Currently in context '${CURRENT_CONTEXT}' using namespace '${CURRENT_NAMESPACE:-"default"}'. Continue? (y/N) " -r -n 1 yn
     case $yn in
         [Yy]* ) ;;
         * ) exit 0;;
     esac
-    echo 
+    echo
 fi
 
-for arg in "$@"
-do
-    if [ ${arg} != "--"* ] && [ -z "${COMMAND}" ]; then
-        COMMAND=${arg}
-    fi
-    NEW_ARGS="${NEW_ARGS} ${arg}"
-done
-
-helm --kube-as-user ${USER} --kube-as-group system:masters ${NEW_ARGS}
+helm --kube-as-user ${USER} --kube-as-group system:masters "$@"
